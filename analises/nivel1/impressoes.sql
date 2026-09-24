8e29e5b6a86e28746453e24390f793ab1bd7d576
Amanda Duaibs <81600175+amandaDuaibs@users.noreply.github.com>
2026-09-24 17:53:47 -0300

    completa a analise de impressoes com consultas reais

--- analises/nivel1/impressoes.sql
+++ analises/nivel1/impressoes.sql
@@ -1,13 +1,32 @@
 --Nível 1
 --Impressões
 
+-- Média de impressões por campanha
+SELECT ROUND(AVG(impressions), 1) AS media_impressoes FROM fact_campaign;
 
+-- Total de impressões
+SELECT SUM(impressions) AS total_impressoes FROM fact_campaign;
 
-SELECT * from marketing_campaign_dataset;
-select * from dim_audience;
-select * from dim_campaign;
-SELECT * from dim_channel;
-SELECT * from dim_date;
-SELECT * from dim_location;
-SELECT * from dim_company;
-SELECT * from fact_campaign;
+-- Top 5 campanhas por impressões
+SELECT fc.id_campaign, fc.impressions
+FROM fact_campaign fc
+ORDER BY fc.impressions DESC
+LIMIT 5;
+
+-- Impressões por canal
+SELECT
+    ch.channel_name,
+    SUM(fc.impressions) AS total_impressoes
+FROM fact_campaign fc
+JOIN dim_channel ch ON fc.id_channel = ch.id_channel
+GROUP BY ch.channel_name
+ORDER BY total_impressoes DESC;
+
+-- Impressões por tipo de campanha
+SELECT
+    c.campaign_type,
+    SUM(fc.impressions) AS total_impressoes
+FROM fact_campaign fc
+JOIN dim_campaign c ON fc.id_campaign = c.id_campaign
+GROUP BY c.campaign_type
+ORDER BY total_impressoes DESC;
